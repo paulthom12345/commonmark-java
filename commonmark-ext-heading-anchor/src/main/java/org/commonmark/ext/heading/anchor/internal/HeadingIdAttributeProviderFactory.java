5b792265a3605eb1a56e232b784740d1917ce073
Paul Thompson <pathompson@atlassian.com>
2016-09-19 21:36:32 +1000

    Add tests for new factory method to ensure new instances
    New test has been added to ensure that any render method invokes a
    totally new instance rather than using the same one.

--- commonmark-ext-heading-anchor/src/main/java/org/commonmark/ext/heading/anchor/internal/HeadingIdAttributeProviderFactory.java
+++ commonmark-ext-heading-anchor/src/main/java/org/commonmark/ext/heading/anchor/internal/HeadingIdAttributeProviderFactory.java
@@ -3,9 +3,6 @@ package org.commonmark.ext.heading.anchor.internal;
 import org.commonmark.html.AttributeProvider;
 import org.commonmark.html.AttributeProviderFactory;
 
-/**
- * Created by pathompson on 19/09/2016.
- */
 public class HeadingIdAttributeProviderFactory implements AttributeProviderFactory {
 
     private HeadingIdAttributeProviderFactory() {
